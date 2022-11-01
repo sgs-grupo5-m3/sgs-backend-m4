d0fa7320a36fbf6f1388cbf93656140fccb43e25
Gutemberg Lamark <gtmbrglamark@gmail.com>
2022-11-01 17:29:56 -0300

    fix: resolucao do validatedBody

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
 import "reflect-metadata";
 
 import express from "express";
 
 import "express-async-errors";
 
+import "reflect-metadata";
+
 import { appRoutes } from "./routes";
 
 import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
-=======
-import express from "express";
-import { appRoutes } from "./routes";
-import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
-import sessionRouter from "./routes/session.routes";
->>>>>>> 8186ec4 (fix: arrumando bugs)
 
 const app = express();
 
