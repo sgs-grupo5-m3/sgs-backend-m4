5102102ba4f8d726b14be75c36fd0628a135e737
João Victor <jvmelo1106@hotmail.com>
2022-11-01 16:47:51 -0300

    fix: arrumando bugs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import "reflect-metadata";
 
 import express from "express";
@@ -7,6 +8,12 @@ import "express-async-errors";
 import { appRoutes } from "./routes";
 
 import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
+=======
+import express from "express";
+import { appRoutes } from "./routes";
+import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
+import sessionRouter from "./routes/session.routes";
+>>>>>>> 8186ec4 (fix: arrumando bugs)
 
 const app = express();
 
