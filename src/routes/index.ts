e41ff199e563297618b9c2057951f23fd302814d
Gutemberg Lamark <gtmbrglamark@gmail.com>
2022-11-01 11:35:05 -0300

    fix: resolvendo conflitos de merge

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
-import patientRouter from "./patient.routes";
+
 import doctorRouter from "./doctor.routes";
+import patientRouter from "./patient.routes";
 
 export const appRoutes = (app: Express) => {
   app.use("/patient", patientRouter());
