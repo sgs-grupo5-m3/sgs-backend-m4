9ede8a4c38e8a322e816aadef03db260d40d9ad1
JoaoDantaas <joao.araujosp@gmail.com>
2022-11-02 14:44:48 -0300

    correção

--- src/controllers/patient/createPatient.controller.ts
+++ src/controllers/patient/createPatient.controller.ts
@@ -2,7 +2,6 @@ import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
 import { IPatientCreate } from "../../interfaces/patient";
 import createPatientService from "../../services/patient/createPatient.service";
-import { instanceToPlain } from "class-transformer";
 
 const createPatientController = async (req: Request, res: Response) => {
   const { name, birth_date, email, password, cpf }: IPatientCreate =
@@ -16,11 +15,7 @@ const createPatientController = async (req: Request, res: Response) => {
     cpf,
   });
 
-<<<<<<< HEAD
   return res.status(201).json(instanceToPlain(patient));
-=======
-  res.status(201).json(instanceToPlain(patient));
->>>>>>> 08f812511ff1163357cdd5a8c1d40805363a5a2b
 };
 
 export default createPatientController;
