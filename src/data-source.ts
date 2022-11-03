be8a5bd22c26a2eff371399482625a5b837ff167
FelipeGeazi <felipegeaziartes@gmail.com>
2022-11-03 19:25:28 -0300

    alteração data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,46 +1,32 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
-/* export const AppDataSource =
+export const AppDataSource =
   process.env.NODE_ENV === "test"
     ? new DataSource({
         type: "sqlite",
-        
         database: ":memory:",
-        entities: ["src/entities/ * * / *.ts"],
+        entities: ["src/entities/**/*.ts"],
         synchronize: true,
       })
     : new DataSource({
         type: "postgres",
-        host: process.env.POSTGRES_HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PWD,
-        database: process.env.POSTGRES_DB,
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
         synchronize: false,
         logging: true,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      });  */
-
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-  synchronize: false,
-  logging: true,
-  entities:
-    process.env.NODE_ENV === "production"
-      ? ["dist/entities/*.js"]
-      : ["src/entities/*.ts"],
-  migrations:
-    process.env.NODE_ENV === "production"
-      ? ["dist/migrations/*.js"]
-      : ["src/migrations/*.ts"],
-});
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/entities/*.js"]
+            : ["src/entities/*.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/migrations/*.js"]
+            : ["src/migrations/*.ts"],
+      });
 
 AppDataSource.initialize()
   .then(() => {
