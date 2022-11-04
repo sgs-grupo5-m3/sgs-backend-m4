194bcbca649b22c2c1b6ed18865f7e9bb7a8798f
Gutemberg Lamark <gtmbrglamark@gmail.com>
2022-11-04 19:12:35 -0300

    feat: configuracao do deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource =
       })
     : new DataSource({
         type: "postgres",
-        url: process.env.HEROKU_POSTGRESQL_CRIMSON_URL,
+        url: process.env.DATABASE_URL,
         ssl:
           process.env.NODE_ENV === "production"
             ? { rejectUnauthorized: false }
