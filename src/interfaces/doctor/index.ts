5c9efadd48b37aead6a419b91c5971044296c9fd
João Victor <jvmelo1106@hotmail.com>
2022-11-07 14:39:05 -0300

    feat: criando specialties

--- src/interfaces/doctor/index.ts
+++ src/interfaces/doctor/index.ts
@@ -1,8 +1,12 @@
-export interface IDoctorRequest{
-    name: string, 
-    birth_date: string, 
-    email: string, 
-    password?: string, 
-    cpf: string, 
-    crm: string
+export interface IDoctorRequest {
+  name: string;
+  birth_date: string;
+  email: string;
+  password?: string;
+  cpf: string;
+  crm: string;
+}
+
+export interface ISpecialtyRequest {
+  name: string;
 }
---
+++ src/middlewares/createSpecialties.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { AppError } from "../errors/appError";
+
+const createSpecialtiesMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name } = req.body;
+
+  const specialtyRepository = AppDataSource.getRepository(Specialties);
+  const specialty = await specialtyRepository.find();
+  const specialtyAleradyExists = specialty.find((elem) => elem.name === name);
+
+  if (specialtyAleradyExists) {
+    throw new AppError(409, "Specialty already exists");
+  }
+
+  next();
+};
+
+export default createSpecialtiesMiddleware;
