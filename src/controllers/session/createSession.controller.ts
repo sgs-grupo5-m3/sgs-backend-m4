2c2a78013117f1fff67f8ac585a469dff4265e0d
FelipeGeazi <felipegeaziartes@gmail.com>
2022-11-07 18:12:42 -0300

    enviando isDoctor no login

--- src/controllers/session/createSession.controller.ts
+++ src/controllers/session/createSession.controller.ts
@@ -5,7 +5,8 @@ import createSessionService from "../../services/session/createSession.service";
 const createSessionController = async (req: Request, res: Response) => {
   const { email, password }: ISession = req.validatedBody as ISession;
   const token = await createSessionService({ email, password });
-  return res.status(200).json({ token });
+  const isDoctor = req.user.isDoctor;
+  return res.status(200).json({ token, isDoctor });
 };
 
 export { createSessionController };
