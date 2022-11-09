8ebd6a6e36046c63106140c0b05fd2ba259a58dd
FelipeGeazi <felipegeaziartes@gmail.com>
2022-11-09 12:32:37 -0300

    corrigindo resposta criação doutor

--- src/services/doctor/createDoctor.service.ts
+++ src/services/doctor/createDoctor.service.ts
@@ -1,30 +1,45 @@
-import { IDoctorRequest } from "../../interfaces/doctor"; 
+import { IDoctorRequest } from "../../interfaces/doctor";
 import { AppDataSource } from "../../data-source";
 import { Doctor } from "../../entities/doctor.entity";
-import { hashSync } from "bcrypt"
+import { hashSync } from "bcrypt";
 import { Specialties } from "../../entities/specialties.entity";
 
-const createDoctorService = async ({ name, birth_date, email, password, cpf, crm, specialtie }: IDoctorRequest): Promise<Doctor> => {
-    const doctorRepository = AppDataSource.getRepository(Doctor);
-    const specialtiesRepository = AppDataSource.getRepository(Specialties)
+const createDoctorService = async ({
+  name,
+  birth_date,
+  email,
+  password,
+  cpf,
+  crm,
+  specialtie,
+}: IDoctorRequest): Promise<Object> => {
+  const doctorRepository = AppDataSource.getRepository(Doctor);
+  const specialtiesRepository = AppDataSource.getRepository(Specialties);
 
-    const specialties = await specialtiesRepository.findOneBy({
-        name: specialtie
-    })
+  const specialties = await specialtiesRepository.findOneBy({
+    name: specialtie,
+  });
 
-    const doctor = await doctorRepository.save({
-        name: name,
-        birth_date: birth_date,
-        email: email,
-        password: hashSync(password!, 10),
-        cpf: cpf,
-        crm: crm,
-        specialties: specialties!
-    });
+  const doctor = await doctorRepository.save({
+    name: name,
+    birth_date: birth_date,
+    email: email,
+    password: hashSync(password!, 10),
+    cpf: cpf,
+    crm: crm,
+    specialties: specialties!,
+  });
 
-    
+  const doctorResponse = {
+    name: name,
+    birth_date: birth_date,
+    email: email,
+    cpf: cpf,
+    crm: crm,
+    specialties: specialtie,
+  };
 
-    return doctor
-}
+  return doctorResponse;
+};
 
 export default createDoctorService;
