0d15cd5b07c25513a3bd3202f676fab4f906d56d
FelipeGeazi <felipegeaziartes@gmail.com>
2022-11-09 14:33:57 -0300

    incluindo a lib cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ import { appRoutes } from "./routes";
 
 import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
 
+var cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
